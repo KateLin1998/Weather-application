8e8ed517d0058198dbc10999d6bd757ccac90557
KateLin1998 <katarina144144@gmail.com>
2022-08-14 13:30:43 +0200

    Updated Index.html, script.js, app.css

--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ let now = new Date();
 
 let h2 = document.querySelector("h2");
 
-let hours = now.getHours(); 
+let hours = now.getHours();
 let minutes = now.getMinutes();
 let date = now.getDate();
 let days = ["Sun", "Mon", "Tue", "Wedn", "Thu", "Fri", "Sat"];
 let day = days[now.getDay()];
-let months = ["Jan", "Feb", "March", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
 let month = months[now.getMonth()];
 let year = now.getFullYear();
 
@@ -15,33 +28,36 @@ h2.innerHTML = `<p>Current time: ${hours}:${minutes} </p> <p> Current day: ${day
 
 function search(event) {
   event.preventDefault();
-  let searchInput = document.querySelector
-  ("#search-text-input");
+  let searchInput = document.querySelector("#search-text-input");
 
   let h3 = document.querySelector("h3");
   if (searchInput.value) {
     h3.innerHTML = `${searchInput.value}`;
   } else {
     h3.innerHTML = null;
-    alert("Please type a city");
+    h3.innerHTML = ``;
   }
+
+  let apiKey = "1b6c0398e1da79a12e6750bada098ecc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric`;
+  let apiUrls = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=imperial`;
+
+  function showTemperature(response) {
+    console.log(response.data);
+    let temperature = Math.round(response.data.main.temp);
+    let h4 = document.querySelector("h4");
+    h4.innerHTML = `${temperature}°C`;
+  }
+
+  function showTemperatures(response) {
+    let temperatures = Math.round(response.data.main.temp);
+    let tempo = document.querySelector("#temp");
+    tempo.innerHTML = `${temperatures} F`;
+  }
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+  axios.get(`${apiUrls}&appid=${apiKey}`).then(showTemperatures);
 }
+
 let form = document.querySelector("form");
 form.addEventListener("submit", search);
-
-function farenheit(event) {
-  event.preventDefault();
-  let link = document.querySelector("h4");
-  link.innerHTML = Math.round((16 * 9 / 5) + 32);
-  }
-
-function celsius(event) {
-  event.preventDefault();
-  let links = document.querySelector("h4");
-  links.innerHTML = Math.round((60 - 32) * 5/9);
-}
-
-  let followLinks = document.querySelector("#celsius");
-  let followLink = document.querySelector("#farenheit");
-  followLinks.addEventListener("click", celsius)
-  followLink.addEventListener("click", farenheit)
